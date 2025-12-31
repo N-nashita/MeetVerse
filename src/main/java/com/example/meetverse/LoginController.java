4b8b03bf1d5157a692d56e2c4b548068b68b9abd
Nashita Binte Hamid <hamid2207067@stud.kuet.ac.bd>
2025-12-31 16:46:12 +0600

    with admin and user dashboard

--- src/main/java/com/example/meetverse/LoginController.java
+++
@@ -1,4 +0,0 @@
-package com.example.meetverse;
-
-public class LoginController {
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.example.meetverse {
 
 
     opens com.example.meetverse to javafx.fxml;
+    opens com.example.meetverse.Controllers to javafx.fxml;
     exports com.example.meetverse;
+    exports com.example.meetverse.Controllers;
 }
